35110f62c84282ae63c23f4203be2427b48d6fcc
Valeria Gutierrez <valeria.gutierrez@ucb.edu.bo>
2026-03-02 23:48:22 -0400

    feat:caso jugador1 anota 2 veces

--- src/tennis.spec.js
+++ src/tennis.spec.js
@@ -15,4 +15,14 @@ describe("Tennis Scorer", () => {
     expect(resultado).toEqual("15 - Love"); //Vericar    ->ASSERT
   });
 
+  //Jugador 1 anota 2 veces jugador 2 en 0=> "30 - Love"
+  it("jugador 1 anota jugador dos en cero", () => {
+    let tennis = new Tennis();   //Inicializar  ->ARRANGE
+    tennis.player1Scores();      //inicializar
+    tennis.player1Scores();      //inicializar
+    let resultado =tennis.score() //Ejecutar metodo a probar  ->ACT
+    expect(resultado).toEqual("30 - Love"); //Vericar    ->ASSERT
+  });
+
+
 });
